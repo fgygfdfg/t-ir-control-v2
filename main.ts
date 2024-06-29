636ed0b050ad44f4bf0a1681dc3e7b99962f1411
fgygfdfg <115881073+fgygfdfg@users.noreply.github.com>
2024-06-29 22:12:39 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,31 @@
+Bit_IR.onPressEvent(RemoteButton.NUM7, function () {
+    basic.showNumber(7)
+})
+Bit_IR.onPressEvent(RemoteButton.NUM6, function () {
+    basic.showNumber(6)
+})
+Bit_IR.onPressEvent(RemoteButton.NUM3, function () {
+    basic.showNumber(3)
+})
+Bit_IR.onPressEvent(RemoteButton.NUM8, function () {
+    basic.showNumber(8)
+})
+Bit_IR.onPressEvent(RemoteButton.NUM4, function () {
+    basic.showNumber(4)
+})
+Bit_IR.onPressEvent(RemoteButton.NUM2, function () {
+    basic.showNumber(2)
+})
+Bit_IR.onPressEvent(RemoteButton.NUM9, function () {
+    basic.showNumber(9)
+})
+Bit_IR.onPressEvent(RemoteButton.NUM0, function () {
+    basic.showNumber(0)
+})
+Bit_IR.onPressEvent(RemoteButton.NUM5, function () {
+    basic.showNumber(5)
+})
+Bit_IR.onPressEvent(RemoteButton.NUM1, function () {
+    basic.showNumber(1)
+})
+Bit_IR.init(Pins.P8)
